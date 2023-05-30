387e93f7d5a813d5d10a98b33a7e38436cecbdda
hzlgrc <135027029+hgducalang@users.noreply.github.com>
2023-05-30 20:57:30 +0800

    Add files via upload

---
+++ pythonbasicarithmetic.py
@@ -0,0 +1,24 @@
+firstNumber = int(input("Enter first number: "))
+secondNumber = int(input("Enter second number: "))
+
+print()
+result = firstNumber+secondNumber
+print(str(firstNumber)+"+"+str(secondNumber)+"="+str(result))
+
+result = firstNumber-secondNumber
+print(str(firstNumber)+"-"+str(secondNumber)+"="+str(result))
+
+result = firstNumber*secondNumber
+print(str(firstNumber)+"*"+str(secondNumber)+"="+str(result))
+
+result = firstNumber/secondNumber
+print(str(firstNumber)+"/"+str(secondNumber)+"="+str(result))
+
+result = firstNumber//secondNumber
+print(str(firstNumber)+"//"+str(secondNumber)+"="+str(result))
+
+result = firstNumber%secondNumber
+print(str(firstNumber)+"%"+str(secondNumber)+"="+str(result))
+
+result = firstNumber**secondNumber
+print(str(firstNumber)+"**"+str(secondNumber)+"="+str(result))
