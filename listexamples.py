80c0e3c97c2bb9113c2d1bec8a40cf69e7e47dd1
hzlgrc <135027029+hgducalang@users.noreply.github.com>
2023-05-31 21:28:21 +0800

    Add files via upload

---
+++ listexamples.py
@@ -0,0 +1,19 @@
+#declaring list
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits)
+
+#printing a specified item in a list using index
+
+#positive index always starts with 0 and from left to right
+print(fruits[0])
+
+#negative index always starts with -1 and from right to left
+print(fruits[-1])
+
+#printing a specified items from start index to end index
+print(fruits[2:5])
+print(fruits[:3])
+print(fruits[2:])
+
+#checking the length or number of items in a list
+print(len(fruits))
